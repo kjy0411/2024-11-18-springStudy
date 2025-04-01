9ba140209e923017c2d63e90299ba68be9e40834
user <user@DESKTOP-KMQKUM1>
2025-04-01 15:11:11 +0900

    AOP(공통 적용)

--- SpringWebProject_5/src/main/java/com/sist/aop/DataBoardAOP.java
+++ SpringWebProject_5/src/main/java/com/sist/aop/DataBoardAOP.java
@@ -1,10 +1,23 @@
 package com.sist.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class DataBoardAOP {
-
+	@Around("execution(* com.sist.web.*Controller.*(..))")
+	//전체 메소드
+	public Object around(ProceedingJoinPoint jp) throws Throwable{
+		Object obj=null;
+		System.out.println("수행 메소드:"+jp.getSignature().getName());
+		//사용자가 요청한 메소드 이름 출력
+		long start=System.currentTimeMillis();
+		obj=jp.proceed();// 실제 호출된 메소드
+		long end=System.currentTimeMillis();
+		System.out.println("수행시간:"+(end-start));
+		return obj;
+	}
 }
