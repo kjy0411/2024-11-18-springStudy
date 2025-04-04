06469b6e08aecfd5156d8fbea6d8bbc27c32e603
user <user@DESKTOP-KMQKUM1>
2025-04-04 17:01:06 +0900

    개인 연습 : tiles 템플릿 적용

---
+++ SpringTilesMyProject/src/main/java/com/sist/web/RouterController.java
@@ -0,0 +1,11 @@
+package com.sist.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class RouterController {
+	@GetMapping("main/main.do")
+	public String main_main() {
+		return "main";
+	}
+}
